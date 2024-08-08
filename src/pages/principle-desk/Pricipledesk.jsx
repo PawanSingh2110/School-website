21d3d2675dc7fa1dfdd2224935470dee601f4538
Pawan <ps7400356@gmail.com>
2024-08-08 17:34:21 +0530

    management

--- src/pages/principle-desk/Pricipledesk.jsx
+++ src/pages/principle-desk/Pricipledesk.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 const Pricipledesk = () => {
   return (
     <div>
       <div className=" title w-full h-48 py-10  text-white bg-orange-500">
         <h1 className="lg:text-5xl text-3xl font-medium text-center ">About</h1>
-        <h1 className="text-center font-bold mt-5">Home > About Us > <span className="font-medium "> Principal's Desk</span> </h1>
+        <h1 className="text-center font-bold mt-5 flex items-center gap-1 justify-center">Home <FaArrowRight/> About Us <FaArrowRight/> <span className="font-medium "> Principal's Desk</span> </h1>
         
       </div>
       <div className="grid grid-cols-1 xl:grid-cols-3">
